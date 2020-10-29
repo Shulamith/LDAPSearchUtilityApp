5d335c8e801a3ce85e2057b8d1e0945dca6c2d8e
Shulamith <shulamit.dashevsky@gmail.com>
2020-10-28 20:21:38 -0400

    Add files via upload

---
+++ build/precache-manifest.767e6b86d08974821bb2116b1da70e42.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a11ebd2da6b3c5da9c663fc02fedfe3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "150a08fe81887338c58f",
+    "url": "/static/css/2.d4e7eff5.chunk.css"
+  },
+  {
+    "revision": "5aa4ac369322040676ed",
+    "url": "/static/css/main.1842e6df.chunk.css"
+  },
+  {
+    "revision": "150a08fe81887338c58f",
+    "url": "/static/js/2.6fd864b5.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.6fd864b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aa4ac369322040676ed",
+    "url": "/static/js/main.b6f9e067.chunk.js"
+  },
+  {
+    "revision": "797d23d1137ad4f481d2",
+    "url": "/static/js/runtime-main.478d1227.js"
+  }
+]);
